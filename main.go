46ae8e78e5f369dfb23b9ee1db74bb89c80dc6d7
Octavian <octaviancir@gmail.com>
2022-12-11 19:05:20 +0200

    added post request

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"protobufPOC/schema"
 
 	"google.golang.org/protobuf/proto"
 )
 
-func handleProtobuf() []byte {
+func getHandleProtobuf() []byte {
 	data, err := proto.Marshal(articles)
 	if err != nil {
 		return nil
@@ -17,7 +20,7 @@ func handleProtobuf() []byte {
 	return data
 }
 
-func handleXML() []byte {
+func getHandleXML() []byte {
 	data, err := xml.Marshal(articles)
 	if err != nil {
 		fmt.Printf("Failed to marshal XML")
@@ -26,7 +29,7 @@ func handleXML() []byte {
 	return data
 }
 
-func handleJson() []byte {
+func getHandlerJson() []byte {
 	data, err := json.Marshal(articles)
 	if err != nil {
 		return nil
@@ -35,15 +38,60 @@ func handleJson() []byte {
 	return data
 }
 
+func postHandleProtobuf(body []byte) {
+	article := &schema.Article{}
+	err := proto.Unmarshal(body, article)
+	articles.Data = append(articles.Data, article)
+	if err != nil {
+		return
+	}
+}
+
+func postHandleJSON(body []byte) {
+	article := &schema.Article{}
+	err := json.Unmarshal(body, article)
+	articles.Data = append(articles.Data, article)
+	if err != nil {
+		return
+	}
+}
+
+func postHandleXML(body []byte) {
+	article := &schema.Article{}
+	err := xml.Unmarshal(body, article)
+	articles.Data = append(articles.Data, article)
+	if err != nil {
+		return
+	}
+}
+
 func articleHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodGet {
 		switch req.Header.Get("Accept") {
 		case "application/x-protobuf":
-			w.Write(handleProtobuf())
+			w.Write(getHandleProtobuf())
 		case "application/xml":
-			w.Write(handleXML())
+			w.Write(getHandleXML())
 		default:
-			w.Write(handleJson())
+			w.Write(getHandlerJson())
+		}
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+
+		switch req.Header.Get("Content-Type") {
+		case "application/x-protobuf":
+			postHandleProtobuf(body)
+		case "application/xml":
+			postHandleXML(body)
+		default:
+			postHandleJSON(body)
 		}
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"protobufPOC/schema"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func makeBenchmark(acceptHeader string) func(b *testing.B) {
+func makeGetBenchmark(acceptHeader string) func(b *testing.B) {
 	return func(b *testing.B) {
 		r, _ := http.NewRequest("GET", "/articles", nil)
 		r.Header.Add("Accept", acceptHeader)
@@ -22,8 +31,62 @@ func makeBenchmark(acceptHeader string) func(b *testing.B) {
 	}
 }
 
-func BenchmarkHandleJson(b *testing.B) {
-	b.Run("Endpoint with JSON: GET /articles", makeBenchmark("application/json"))
-	b.Run("Endpoint with XML: GET /articles", makeBenchmark("application/xml"))
-	b.Run("Endpoint with Protobuf: GET /articles", makeBenchmark("application/x-protobuf"))
+type marshaller func(v any) ([]byte, error)
+
+func proto_marshal(v any) ([]byte, error) {
+	return proto.Marshal(v.(proto.Message))
+}
+
+func makePostBenchmark(contentType string, fn marshaller) func(b *testing.B) {
+	article := &schema.Article{
+		Id:    200,
+		Title: "Lorem Ipsum",
+		Authors: []*schema.Author{
+			{
+				FirstName: "Dennis",
+				LastName:  "Ritchie",
+			},
+			{
+				FirstName: "Brian",
+				LastName:  "Kernighan",
+			}},
+		Content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+		Categories: []uint32{
+			uint32(3),
+		},
+		State:        &Draft,
+		DateCreated:  timestamppb.New(time.Now()),
+		DateModefied: timestamppb.Now(),
+	}
+	data, err := fn(article)
+	if err != nil {
+		return nil
+	}
+	reader := bytes.NewReader(data)
+
+	return func(b *testing.B) {
+		r, _ := http.NewRequest("POST", "/articles", reader)
+		r.Header.Add("Content-Type", contentType)
+		w := httptest.NewRecorder()
+		handler := http.HandlerFunc(articleHandler)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			handler.ServeHTTP(w, r)
+		}
+	}
+}
+
+func BenchmarkHandleGetRequest(b *testing.B) {
+	b.Run("Endpoint with JSON: GET /articles", makeGetBenchmark("application/json"))
+	b.Run("Endpoint with XML: GET /articles", makeGetBenchmark("application/xml"))
+	b.Run("Endpoint with Protobuf: GET /articles", makeGetBenchmark("application/x-protobuf"))
+}
+
+func BenchmarkHandlePostRequest(b *testing.B) {
+	b.Run("Endpoint with JSON: POST /articles", makePostBenchmark("application/json", json.Marshal))
+	b.Run("Endpoint with XML: POST /articles", makePostBenchmark("application/xml", xml.Marshal))
+	b.Run("Endpoint with Protobuf: POST /articles", makePostBenchmark("application/x-protobuf", proto_marshal))
 }
